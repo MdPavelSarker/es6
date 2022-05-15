f796dbbf9e2edff82c87ddadd12d29f0fc599d8b
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-15 23:34:48 +0600

    default function

---
+++ default.js
@@ -0,0 +1,11 @@
+function add(num1 , num2 = 13){
+    // if( num2 == undefined){
+    //     num2 = 0;
+    // }
+
+    // num2 = num2 || 0;
+
+    return num1 + num2;
+}
+const total =  add(39 , 2);
+console.log(total);
