9a150ba611677505f2f6afd5e536a09654e8c4cc
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-15 23:37:28 +0600

    letconst

---
+++ letconst.js
@@ -0,0 +1,18 @@
+const name = "Hablu";
+console.log(name);
+
+const nums = [12,45];
+nums[1] = 88;
+nums.push(13);
+console.log(nums);
+
+let patinetName = "Korimer Bap";
+patinetName = "Rozonir Ma";
+console.log(patinetName);
+
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i);
