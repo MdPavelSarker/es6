a1f65252ef37251887a28dd3733f6e015266f31e
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-15 23:38:28 +0600

    template

---
+++ template.js
@@ -0,0 +1,17 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+// const fullName = firstName + " " + lastName + " Is a honest persron";
+const fullName2 =`${firstName} ${20+30+50} Is a good boy.`;
+
+//  const multiLine1 = "Hey whats up\n"
+//  +"How did you feel\n"
+//  +"Whats going on!\n"; 
+
+const multiLine = ` Hey whats up
+How did you feel
+Whats going on!
+Get out from here`; 
+
+console.log(fullName2);
+console.log(multiLine);
+//console.log(multiLine1);
