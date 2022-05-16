23033a55294ac736cfae1e7961718728b35fc40c
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-16 23:09:08 +0600

    class pratice

---
+++ class.js
@@ -0,0 +1,17 @@
+class Student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Kolimunnesa Schlool"
+
+    }
+
+}
+
+const student1 = new Student( 142362316 , "Nasir");
+const student2 = new Student(142362306, "Sojib");
+console.log(student1, student2);
+
+
+
+
